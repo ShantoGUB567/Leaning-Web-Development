293c27d09860c21f40fcc2fd86c9511cf5b60ac0
ShantoGUB567 <shahriarshanto221002567@gmail.com>
2024-11-10 00:34:21 +0600

    Initially upload

---
+++ CSS/Practice 1/index.js
@@ -0,0 +1,19 @@
+// alert('Hello Shanto !');
+
+// var fname= prompt("Enter your first name: ");
+// var lname= prompt("Enter your last name: ");
+
+// document.write("<h1>Hello "+ fname + lname +"</h1>");
+
+// var num1=10;
+// var num2=5;
+
+// document.write(setTimeout(multiply(num1 * num2), 2000));
+// document.write(setTimeout(multiply, 2000, num1, num2));
+
+var name= prompt("Enter your name: ");
+
+if(name)
+    document.write("<h1>Hello "+ name +" !<br>Welcome to our Website</h1>");
+else
+    document.write("<h1>Hello !<br>Welcome to our Website</h1>");
